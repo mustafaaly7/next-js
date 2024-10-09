8ae0b31b1ed64d5bbb2ab940b216d490dfb4ca7f
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-09 19:08:11 +0500

    route.js in next js

---
+++ class4/my-app/app/(root)/categories/loading.js
@@ -0,0 +1,6 @@
+export default function Home() {
+    return (
+  
+     <h1 className={"text-center text-6xl my-6"}>Home Page</h1>
+    );
+  }
---
+++ class4/my-app/app/api/products/page.js
@@ -0,0 +1,9 @@
+
+
+
+export default function Home() {
+    return (
+  
+     <h1 className={"text-center text-6xl my-6"}>Products</h1>
+    );
+  }
