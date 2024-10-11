0325db5b19b585d47de485cea75ab196cc62ee6c
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-11 18:21:21 +0500

    class 4 request and response postman POST AND GET

--- class4/my-app/app/api/products/route.js
+++ class4/my-app/app/api/products/route.js
@@ -21,6 +21,11 @@ data: quotes,
 export async function POST(request){
 
 const myEdit = await request.json() // api ko request bhejte hain 
+//i saved it in var name myEdit to mai jo bh quote postman say bhejunga woh idhar aiga request mai hum request bhejte hain 
+// and ata response hai 
+
+console.log(myEdit);
+
 quotes.push(myEdit)
   return Response.json({
     //humein response ata hai 
